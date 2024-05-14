ec38a6952056f2d4d6b0dffd25736df71e55046a
Hubert-Vecchioli <hubert.vecchioli@gmail.com>
2024-05-15 01:42:59 +0200

    Pre-work

---
+++ libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 01:01:54 by hvecchio          #+#    #+#             */
+/*   Updated: 2024/05/15 01:42:32 by hvecchio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *str);
+void	*ft_memset(void *ptr, int x, size_t n);
+void	ft_bzero(void *ptr, size_t n);
+
+#endif
