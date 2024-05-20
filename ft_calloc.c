7ccdc3a5ecb319219b5263a3ae240f49c5bfd1bf
Hubert-Vecchioli <hubert.vecchioli@gmail.com>
2024-05-20 11:49:11 +0200

    Protect malloc overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 09:41:36 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/05/17 17:19:37 by hvecchio         ###   ########.fr       */
+/*   Updated: 2024/05/20 11:46:07 by hvecchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t n, size_t size)
 {
 	void	*result;
 
+	if (size != 0 && n > (size_t) - 1 / size)
+		return (NULL);
 	result = malloc(n * size);
 	if (result == NULL)
 		return (NULL);
